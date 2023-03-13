bb7be8ce0d116ec8ea575fdd606f7ca1ceb2156a
Sandeep <sandeepjayabal41@gmail.com>
2023-03-13 16:27:13 -0400

    First Commit

--- app/src/main/java/com/project/thevoice/MainActivity.java
+++ app/src/main/java/com/project/thevoice/MainActivity.java
@@ -3,13 +3,22 @@ package com.project.thevoice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity
 {
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button mapviewbutton= findViewById(R.id.MapViewButton);
+        mapviewbutton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+            }
+
     }
+
 }
