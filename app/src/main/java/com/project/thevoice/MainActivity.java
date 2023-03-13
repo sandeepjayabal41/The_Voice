3a59cce7a5f807f229d972195d95f2d49824396e
Sandeep <sandeepjayabal41@gmail.com>
2023-03-13 19:39:20 -0400

    Updates

--- app/src/main/java/com/project/thevoice/MainActivity.java
+++ app/src/main/java/com/project/thevoice/MainActivity.java
@@ -6,6 +6,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -16,6 +19,8 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
 
         final Button mapviewbutton= findViewById(R.id.MapViewButton);
+        final TextView forgotPasswordLink = findViewById(R.id.forgot_your_password_);
+
         mapviewbutton.setOnClickListener(new View.OnClickListener()
         {
             public void onClick(View v)
@@ -23,6 +28,14 @@ public class MainActivity extends AppCompatActivity
                 startActivity(new Intent(v.getContext(), MapsActivity.class));
             }
         });
+
+        forgotPasswordLink.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //Reset command
+            }
+        });
     }
 
-}
+
+
+    }
