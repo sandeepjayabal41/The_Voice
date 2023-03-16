653c1442b2b70cc411e6975924c6cfa8fa066400
Sandeep <sandeepjayabal41@gmail.com>
2023-03-16 02:00:06 -0400

    Updates

--- app/src/main/java/com/project/thevoice/MapsActivity.java
+++ app/src/main/java/com/project/thevoice/MapsActivity.java
@@ -7,9 +7,9 @@ import androidx.fragment.app.FragmentActivity;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
-import android.location.LocationRequest;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -19,15 +19,18 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.project.thevoice.databinding.ActivityMapsBinding;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 {
+    FusedLocationProviderClient mFusedLocationClient;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-
         com.project.thevoice.databinding.ActivityMapsBinding binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
@@ -37,22 +40,29 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         assert mapFragment != null;
         mapFragment.getMapAsync(this);
     }
-
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap)
     {
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             googleMap.setMyLocationEnabled(true);
-            googleMap.setBuildingsEnabled(true);
-            return;
         }
         else
         {
             ActivityCompat.requestPermissions(this, new String[]
                             {android.Manifest.permission.ACCESS_FINE_LOCATION},1);
         }
-
-
-
+        mFusedLocationClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+            @Override
+            public void onComplete(@NonNull Task<Location> task)
+            {
+                Location location = task.getResult();
+                double latitude = location.getLatitude();
+                double longitude = location.getLongitude();
+                LatLng loc = new LatLng(latitude, longitude);
+                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(loc,15));
+                googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+            }
+        });
     }
 }
