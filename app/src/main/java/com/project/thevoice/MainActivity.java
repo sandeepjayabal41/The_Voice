ce400158c3c4157ef2c3f1c2b2654678bb3346bc
Sandeep <sandeepjayabal41@gmail.com>
2023-03-17 02:09:18 -0400

    Updates

--- app/src/main/java/com/project/thevoice/MainActivity.java
+++ app/src/main/java/com/project/thevoice/MainActivity.java
@@ -1,5 +1,7 @@
 package com.project.thevoice;
 
+import static android.icu.text.ListFormatter.Type.OR;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,21 +11,39 @@ import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity
 {
+    FirebaseAuth mFirebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         getSupportActionBar().hide();
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
+
         final Button mapviewbutton= findViewById(R.id.MapViewButton);
+        final Button loginbutton = findViewById(R.id.LoginButton);
+
+        final EditText emailid = findViewById(R.id.EmailAddress);
+        final EditText password = findViewById(R.id.Password);
+
+
+        loginbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+
+            }
+        });
 
         mapviewbutton.setOnClickListener(new View.OnClickListener()
         {
@@ -32,7 +52,5 @@ public class MainActivity extends AppCompatActivity
                 startActivity(new Intent(v.getContext(), MapsActivity.class));
             }
         });
-
-
     }
     }
