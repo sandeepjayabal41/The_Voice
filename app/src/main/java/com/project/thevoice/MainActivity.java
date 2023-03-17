6116db61cace5e81c2fc4c0bd36bdefedd080a05
Sandeep <sandeepjayabal41@gmail.com>
2023-03-17 01:46:27 -0400

    Updates

--- app/src/main/java/com/project/thevoice/MainActivity.java
+++ app/src/main/java/com/project/thevoice/MainActivity.java
@@ -3,9 +3,13 @@ package com.project.thevoice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.RenderEffect;
+import android.graphics.Shader;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -19,7 +23,6 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-
         final Button mapviewbutton= findViewById(R.id.MapViewButton);
 
         mapviewbutton.setOnClickListener(new View.OnClickListener()
@@ -32,7 +35,4 @@ public class MainActivity extends AppCompatActivity
 
 
     }
-
-
-
     }
--- app/src/main/java/com/project/thevoice/MapsActivity.java
+++ app/src/main/java/com/project/thevoice/MapsActivity.java
@@ -1,5 +1,7 @@
 package com.project.thevoice;
 
+import static com.google.android.gms.location.Priority.PRIORITY_HIGH_ACCURACY;
+
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
@@ -7,8 +9,10 @@ import androidx.fragment.app.FragmentActivity;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -22,6 +26,10 @@ import com.project.thevoice.databinding.ActivityMapsBinding;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 {
     FusedLocationProviderClient mFusedLocationClient;
+    private LocationRequest locationRequest;
+    public Location location = null;
+    public double latitude,longitude;
+    public LatLng loc = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -36,10 +44,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         assert mapFragment != null;
         mapFragment.getMapAsync(this);
     }
+
+
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap)
     {
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             googleMap.setMyLocationEnabled(true);
         }
@@ -48,14 +59,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             ActivityCompat.requestPermissions(this, new String[]
                             {android.Manifest.permission.ACCESS_FINE_LOCATION},1);
         }
-        mFusedLocationClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+
+        Task<Location> getCurrentLocation = mFusedLocationClient.getCurrentLocation(PRIORITY_HIGH_ACCURACY,null);
+        getCurrentLocation.addOnCompleteListener(new OnCompleteListener<Location>()
+        {
             @Override
-            public void onComplete(@NonNull Task<Location> task)
+            public void onComplete(@NonNull Task getCurrentLocation)
             {
-                Location location = task.getResult();
-                double latitude = location.getLatitude();
-                double longitude = location.getLongitude();
-                LatLng loc = new LatLng(latitude, longitude);
+                location = (Location) getCurrentLocation.getResult();
+                latitude = location.getLatitude();
+                longitude = location.getLongitude();
+                loc = new LatLng(latitude, longitude);
                 googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(loc,15));
                 googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
             }
