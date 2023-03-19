5d90bdc2aa280c47ddbb3854fddee2ce64915b5c
Sandeep <sandeepjayabal41@gmail.com>
2023-03-19 15:13:13 -0400

    Updates

--- app/src/main/java/com/project/thevoice/SignUpActivity.java
+++ app/src/main/java/com/project/thevoice/SignUpActivity.java
@@ -3,12 +3,26 @@ package com.project.thevoice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.project.thevoice.databinding.ActivitySignUpBinding;
 
 public class SignUpActivity extends AppCompatActivity {
-
+    ActivitySignUpBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding=  ActivitySignUpBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_sign_up);
+
+        binding.SignUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String firstname = binding.FirstName.getText().toString();
+                String lastname = binding..getText().toString();
+                String firstname = binding.FirstName.getText().toString();
+                String firstname = binding.FirstName.getText().toString();
+            }
+        });
     }
 }
