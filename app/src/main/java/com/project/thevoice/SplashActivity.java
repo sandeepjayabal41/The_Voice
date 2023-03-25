bcd5ab08456dd6b02d327700f954dbd831a305f7
Muthukeerthana Kaliappan <muthukeerthana98@gmail.com>
2023-03-25 17:59:04 -0400

    splash screen

--- app/src/main/java/com/project/thevoice/SplashActivity.java
+++ app/src/main/java/com/project/thevoice/SplashActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ImageView;
 
 public class SplashActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,8 +14,7 @@ public class SplashActivity extends Activity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-                startActivity(intent);
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
                 finish();
             }
         }, 3000); // The delay time in milliseconds (in this example, 3 seconds)
