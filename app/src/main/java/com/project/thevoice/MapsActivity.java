9388f8fb0787ea8d484e8b8048b96dd161777643
Sandeep <sandeepjayabal41@gmail.com>
2023-03-25 15:13:45 -0400

    Updates

--- app/src/main/java/com/project/thevoice/MapsActivity.java
+++ app/src/main/java/com/project/thevoice/MapsActivity.java
@@ -9,7 +9,9 @@ import androidx.fragment.app.FragmentActivity;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -18,21 +20,23 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.tasks.Task;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.model.PlaceLikelihood;
+import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest;
+import com.google.android.libraries.places.api.net.PlacesClient;
 import com.project.thevoice.databinding.ActivityMapsBinding;
 
 import java.util.Arrays;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
-{
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
     FusedLocationProviderClient mFusedLocationClient;
     private PlacesClient mPlacesClient;
     Location location = null;
-    double latitude,longitude;
+    double latitude, longitude;
     LatLng loc = null;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         com.project.thevoice.databinding.ActivityMapsBinding binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -46,43 +50,50 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
 
     @Override
-    public void onMapReady(@NonNull GoogleMap googleMap)
-    {
+    public void onMapReady(@NonNull GoogleMap googleMap) {
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             googleMap.setMyLocationEnabled(true);
-        }
-        else
-        {
+        } else {
             ActivityCompat.requestPermissions(this, new String[]
-                            {android.Manifest.permission.ACCESS_FINE_LOCATION},1);
+                    {android.Manifest.permission.ACCESS_FINE_LOCATION}, 1);
         }
 
-        Task<Location> getCurrentLocation = mFusedLocationClient.getCurrentLocation(PRIORITY_HIGH_ACCURACY,null);
+        Task<Location> getCurrentLocation = mFusedLocationClient.getCurrentLocation(PRIORITY_HIGH_ACCURACY, null);
         getCurrentLocation.addOnCompleteListener(getCurrentLocation1 -> {
             location = (Location) getCurrentLocation1.getResult();
             latitude = location.getLatitude();
             longitude = location.getLongitude();
             loc = new LatLng(latitude, longitude);
-            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(loc,15));
+            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(loc, 15));
             googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
         });
 
-        googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener()
-        {
+        googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
             @Override
-            public void onMapClick(LatLng latLng)
-            {
+            public void onMapClick(LatLng latLng) {
                 getPlaceId(latLng);
             }
         });
+    }
 
-        private void getPlaceId(LatLng latLng) {
+    private void getPlaceId(LatLng latLng)
+    {
         FindCurrentPlaceRequest request = FindCurrentPlaceRequest.builder(Arrays.asList(Place.Field.PLACE_ID))
                 .location(latLng)
                 .build();
 
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
         mPlacesClient.findCurrentPlace(request).addOnSuccessListener((response) -> {
             for (PlaceLikelihood placeLikelihood : response.getPlaceLikelihoods()) {
                 String placeId = placeLikelihood.getPlace().getId();
@@ -97,18 +108,4 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         });
     }
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-}
 }
