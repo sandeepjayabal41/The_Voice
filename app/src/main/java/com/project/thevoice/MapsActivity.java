7ca7bd318ac5a217990efc8f5bcf8c6d8e8cd718
Sandeep <sandeepjayabal41@gmail.com>
2023-03-28 17:31:19 -0400

    Updates

--- app/src/main/java/com/project/thevoice/MapsActivity.java
+++ app/src/main/java/com/project/thevoice/MapsActivity.java
@@ -3,17 +3,23 @@ package com.project.thevoice;
 import static com.google.android.gms.location.Priority.PRIORITY_HIGH_ACCURACY;
 
 import androidx.annotation.NonNull;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
+import androidx.viewbinding.ViewBindings;
 
 import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
-import android.util.Log;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
-import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -22,22 +28,16 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.tasks.Task;
-import com.google.android.libraries.places.api.Places;
-import com.google.android.libraries.places.api.model.Place;
-import com.google.android.libraries.places.api.model.PlaceLikelihood;
-import com.google.android.libraries.places.api.net.FetchPlaceRequest;
-import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest;
-import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.project.thevoice.databinding.ActivityMapsBinding;
 
-import java.util.Arrays;
-import java.util.Collections;
+import java.io.IOException;
 import java.util.List;
-import java.util.Objects;
+import java.util.Locale;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
+{
     FusedLocationProviderClient mFusedLocationClient;
-    private PlacesClient mPlacesClient;
     Location location = null;
     double latitude, longitude;
     LatLng loc = null;
@@ -46,29 +46,23 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        com.project.thevoice.databinding.ActivityMapsBinding binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        ActivityMapsBinding binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
-        Places.initialize(getApplicationContext(), "AIzaSyCd83F00FOzhpe2nxTAKivZ7wKoGlGeWCY");
-        mPlacesClient = Places.createClient(this);
-
+    }
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap)
+    {
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         assert mapFragment != null;
         mapFragment.getMapAsync(this);
-    }
 
-
-    @Override
-    public void onMapReady(@NonNull GoogleMap googleMap)
-    {
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             googleMap.setMyLocationEnabled(true);
-        } else
-        {
+        } else {
             ActivityCompat.requestPermissions(this, new String[]
                     {android.Manifest.permission.ACCESS_FINE_LOCATION}, 1);
         }
@@ -81,68 +75,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             loc = new LatLng(latitude, longitude);
             googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(loc, 15));
             googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-        });
-
-        googleMap.setOnMapClickListener(latLng -> getPlaceId(latLng,googleMap));
-    }
-
-    private void getPlaceId(LatLng latLng,GoogleMap googleMap)
-    {
-        FindCurrentPlaceRequest request = FindCurrentPlaceRequest.builder(Collections.singletonList(Place.Field.ID))
-                .build();
-
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            googleMap.setMyLocationEnabled(true);
-        } else {
-            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-        }
-
-        mPlacesClient.findCurrentPlace(request).addOnSuccessListener((response) -> {
-
-            for (PlaceLikelihood placeLikelihood : response.getPlaceLikelihoods()) {
-                String placeId = placeLikelihood.getPlace().getId();
-
-                Log.d("MapsActivity", "Place ID: " + placeId);
-
-                // Get the details of the place
-                List<Place.Field> placeFields = Arrays.asList(
-                        Place.Field.NAME,
-                        Place.Field.ADDRESS,
-                        Place.Field.PHONE_NUMBER,
-                        Place.Field.WEBSITE_URI,
-                        Place.Field.RATING
-                );
-                FetchPlaceRequest fetchPlaceRequest = FetchPlaceRequest.builder(placeId, placeFields).build();
-                mPlacesClient.fetchPlace(fetchPlaceRequest).addOnSuccessListener((place) ->
-                {
-                    // Show the place details in the UI
-                    TextView nameTextView = findViewById(R.id.place_name);
-                    RatingBar ratingBar = findViewById(R.id.place_rating);
-                    TextView addressTextView = findViewById(R.id.place_address);
-                    TextView phoneTextView = findViewById(R.id.place_phone_number);
-                    TextView websiteTextView = findViewById(R.id.place_website);
-
-                    nameTextView.setText(place.getPlace().getName());
-                    ratingBar.setRating(Objects.requireNonNull(place.getPlace().getRating()).floatValue());
-                    addressTextView.setText(place.getPlace().getAddress());
-                    phoneTextView.setText(place.getPlace().getPhoneNumber());
-                    websiteTextView.setText(Objects.requireNonNull(place.getPlace().getWebsiteUri()).toString());
-                }).addOnFailureListener((exception) ->
-                {
-                    if (exception instanceof ApiException)
-                    {
-                        ApiException apiException = (ApiException) exception;
-                        int statusCode = apiException.getStatusCode();
-                        Log.e("MapsActivity", "Place not found: " + exception.getMessage());
-                    }
-                });
-            }
-        }).addOnFailureListener((exception) -> {
-            if (exception instanceof ApiException) {
-                ApiException apiException = (ApiException) exception;
-                int statusCode = apiException.getStatusCode();
-                Log.e("MapsActivity", "Place not found: " + exception.getMessage());
-            }
         });
     }
 }
